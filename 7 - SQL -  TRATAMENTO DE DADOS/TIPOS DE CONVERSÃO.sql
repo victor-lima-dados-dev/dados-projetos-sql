b6c3feb8a125882927107dab09cdbd6e78e7a74e
Victor Lima <joaovictoranalistadev@gmail.com>
2025-03-06 10:58:44 -0300

    tratamento de dados e manipulação

---
+++ 7 - SQL -  TRATAMENTO DE DADOS/TIPOS DE CONVERSÃO.sql
@@ -0,0 +1,43 @@
+-- TIPOS DE CONVERSÃO ##############################################################
+-- Operador ::
+-- CAST
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) Conversão de texto em data
+-- Corrija a query abaixo utilizando o operador ::
+select '2021-10-01' ::date - '2021-02-01'::date
+
+
+
+select nome_coluna::date
+from nome_tabela
+
+-- (Exemplo 2) Conversão de texto em número
+-- Corrija a query abaixo utilizando o operador ::
+select '100'::numeric - '10'::numeric
+
+
+-- (Exemplo 3) Conversão de número em texto
+-- Corrija a query abaixo utilizando o operador ::
+
+select replace(112122::text ,'1','A') 
+
+-- Priemiro componente (112122) componente 
+-- Segundo (1) o que será subustituido
+--Terceiro (A) valor que subustituirá o segundo componente
+
+
+-- (Exemplo 4) Conversão de texto em data
+-- Corrija a query abaixo utilizando a função CAST
+
+select cast('2021-10-01' as date) - cast('2021-02-01' as date)
+
+
+-- RESUMO ##########################################################################
+
+-- (1) O operador :: e o CAST() são funções utilizadas para converter um dado para 
+-- a unidade desejada. 
+-- (2) O operador :: é mais "clean", porém, em algumas ocasiões não funciona, sendo
+-- necessário utilizar a função CAST()
+-- (3) Use o Guia de comandos para consultar a lista de unidades mais utilizadas
+
